eed9674c34e943e312df1fb6ce969abecf53dcb2
Roman <romankr1997@gmail.com>
2022-09-24 14:46:59 +0200

    practica 10 finalizada

--- 1ev/03_practicas/practica_10.php
+++ 1ev/03_practicas/practica_10.php
@@ -7,104 +7,69 @@
     $envio = false;
     $error = false;
 
-
-    if($nombre != "" && $empresa != "" && $responsable != "" && $fecha != ""){
-        $envio = true;
-    }else{
-        $error = true;
+    
+    if(isset($_GET['nombre']) || isset($_GET['empresa']) || isset($_GET['responsable']) || isset($_GET['fecha'])){
+        if($nombre == "" || $empresa == "" || $responsable == "" || $fecha == ""){
+            $error = true;
+        }else{
+            $envio = true;
+        }
     }
 
 
     ob_end_clean();
     require('pruebas/fpdf184/fpdf.php');
 
-    // $pdf = new FPDF();
-                
-    // $pdf->AddPage();
-    // $pdf->SetFont('Arial', 'B', 18);
-    // $pdf->Cell(0,20,'Carta de recomendacion', 1, 0, 'C');
-    // $pdf->Ln();
-    // $pdf->Ln();
-
-    // $pdf->SetFont('Arial', 'B', 14);
-    // $pdf->Multicell(0,15,"Nombre: $nombre" . "\nEmpresa: $empresa" . "\nResponsable: $responsable" . "\nFecha: $fecha");
-
     class PDF extends FPDF
     {
-    function Header()
-    {
-        global $title;
-    
-        // Arial bold 15
-        $this->SetFont('Arial','B',15);
-        // Calculamos ancho y posición del título.
-        $w = $this->GetStringWidth($title)+6;
-        $this->SetX((210-$w)/2);
-        // Colores de los bordes, fondo y texto
-        $this->SetDrawColor(0,80,180);
-        $this->SetFillColor(230,230,0);
-        $this->SetTextColor(220,50,50);
-        // Ancho del borde (1 mm)
-        $this->SetLineWidth(1);
-        // Título
-        $this->Cell($w,9,$title,1,1,'C',true);
-        // Salto de línea
-        $this->Ln(10);
-    }
-    
-    function Footer()
-    {
-        // Posición a 1,5 cm del final
-        $this->SetY(-15);
-        // Arial itálica 8
-        $this->SetFont('Arial','I',8);
-        // Color del texto en gris
-        $this->SetTextColor(128);
-        // Número de página
-        $this->Cell(0,10,'Página '.$this->PageNo(),0,0,'C');
-    }
-    
-    function ChapterTitle($num, $label)
-    {
-        // Arial 12
-        $this->SetFont('Arial','',12);
-        // Color de fondo
-        $this->SetFillColor(200,220,255);
-        // Título
-        $this->Cell(0,6,"Capítulo $num : $label",0,1,'L',true);
-        // Salto de línea
-        $this->Ln(4);
-    }
-    
-    function ChapterBody($file)
-    {
-        // Leemos el fichero
-        $txt = file_get_contents($file);
-        // Times 12
-        $this->SetFont('Times','',12);
-        // Imprimimos el texto justificado
-        $this->MultiCell(0,5,$txt);
-        // Salto de línea
-        $this->Ln();
-        // Cita en itálica
-        $this->SetFont('','I');
-        $this->Cell(0,5,'(fin del extracto)');
-    }
-    
-    function PrintChapter($num, $title, $file)
-    {
-        $this->AddPage();
-        $this->ChapterTitle($num,$title);
-        $this->ChapterBody($file);
-    }
+        function Header()
+        {
+            global $title;
+        
+            $this->SetFont('Arial','B',15);
+            $w = $this->GetStringWidth($title)+6;
+            $this->SetX((210-$w)/2);
+            $this->SetDrawColor(8, 253, 216);
+            $this->SetFillColor(29, 29, 29);
+            $this->SetTextColor(8, 253, 216);
+            $this->SetLineWidth(1);
+            $this->Cell($w,9,$title,1,1,'C',true);
+            $this->Ln(10);
+        }
+        
+        function Footer()
+        {
+            $this->SetY(-15);
+            $this->SetFont('Arial','I',8);
+            $this->SetTextColor(128);
+            $this->Cell(0,10,'Pagina '.$this->PageNo(),0,0,'C');
+        }
+        
+        function Section($campo, $var)
+        {
+            $this->SetFont('Arial','',12);
+            $this->Cell(35,6,"$campo:",0,0,'L',false);
+            $this->SetFont('Arial','B',12);
+            $this->Cell(50,6,"$var",0,0,'L',false);
+            $this->Ln();
+        }
+        
+        function PrintSection($campo, $var)
+        {
+            $this->Section($campo, $var);
+        }
     }
     
     $pdf = new PDF();
-    $title = '20000 Leguas de Viaje Submarino';
+    
+    $title = 'Carta de recomendacion';
     $pdf->SetTitle($title);
-    $pdf->SetAuthor('Julio Verne');
-    $pdf->PrintChapter(1,'UN RIZO DE HUIDA','20k_c1.txt');
-    $pdf->PrintChapter(2,'LOS PROS Y LOS CONTRAS','practica_10.txt');
+    $pdf->SetAuthor('Roman');
+    $pdf->AddPage();
+    $pdf->PrintSection('Nombre', $nombre);
+    $pdf->PrintSection('Empresa', $empresa);
+    $pdf->PrintSection('Responsable', $responsable);
+    $pdf->PrintSection('Fecha', $fecha);
 
     if($envio){
         $pdf->Output();
@@ -127,19 +92,19 @@
             <form class="formulario" action="" method="get">
                 <ul class="form-ul">
                     <li class="form-ul-li">
-                        <input class="input input-text" type="text" name="nombre" id="nombre" value="<?= $nombre ?>" placeholder="Introduce tu NOMBRE" required>
+                        <input class="input input-text" type="text" name="nombre" id="nombre" value="<?= $nombre ?>" placeholder="Introduce tu NOMBRE">
                         <label class="label-lane" for="nombre"></label> 
                     </li>
                     <li class="form-ul-li">
-                        <input class="input input-text" type="text" name="empresa" id="empresa" value="<?= $empresa ?>" placeholder="Introduce la EMPRESA" required>
+                        <input class="input input-text" type="text" name="empresa" id="empresa" value="<?= $empresa ?>" placeholder="Introduce la EMPRESA">
                         <label class="label-lane" for="empresa"></label> 
                     </li>
                     <li class="form-ul-li">
-                        <input class="input input-text" type="text" name="responsable" id="responsable" value="<?= $responsable ?>" placeholder="Introduce tu RESPONSABLE" required>
+                        <input class="input input-text" type="text" name="responsable" id="responsable" value="<?= $responsable ?>" placeholder="Introduce tu RESPONSABLE">
                         <label class="label-lane" for="responsable"></label> 
                     </li>
                     <li class="form-ul-li">
-                        <input class="input input-text" type="text" name="fecha" id="fecha" value="<?= $fecha ?>" placeholder="Introduce tu FECHA" required>
+                        <input class="input input-text" type="date" name="fecha" id="fecha" value="<?= $fecha ?>" placeholder="Introduce tu FECHA">
                         <label class="label-lane" for="fecha"></label> 
                     </li>
                 </ul>
@@ -147,7 +112,7 @@
                 <button class="button" type="submit">GENERAR</button>
             </form>
             <?php if ($error) { ?>
-                <!-- <h3 class="error-message">ERROR: rellena todos los campos</h3> -->
+                <h3 class="error-message">ERROR: rellena todos los campos</h3>
             <?php } ?>
         </div>
     </div>
