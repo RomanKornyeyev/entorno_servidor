c7093d25db464bc58581ed0aa9170fd9c042710c
Roman <romankr1997@gmail.com>
2022-09-24 20:07:19 +0200

    añadido scroll reveal

--- 1ev/03_practicas/practica_10.php
+++ 1ev/03_practicas/practica_10.php
@@ -109,6 +109,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="./js/scrollreveal-lib.js"></script>
+    <script src="./js/scrollreveal.js" defer=""></script>
     <link rel="stylesheet" href="./css/estilo_10.css">
     <title>Generar PDF</title>
 </head>
