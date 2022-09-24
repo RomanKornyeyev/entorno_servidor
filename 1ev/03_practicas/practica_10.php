3de67276014093696f7c21a16ee00ffe4fb39740
Roman <romankr1997@gmail.com>
2022-09-24 19:20:52 +0200

    practica 10 finalizada++

--- 1ev/03_practicas/practica_10.php
+++ 1ev/03_practicas/practica_10.php
@@ -1,6 +1,9 @@
 <?php
 
     $nombre = $_GET['nombre'];
+    $email = $_GET['email'];
+    $ciudad = $_GET['ciudad'];
+    $telefono = $_GET['telefono'];
     $empresa = $_GET['empresa'];
     $responsable = $_GET['responsable'];
     $fecha = $_GET['fecha'];
@@ -8,8 +11,10 @@
     $error = false;
 
     
-    if(isset($_GET['nombre']) || isset($_GET['empresa']) || isset($_GET['responsable']) || isset($_GET['fecha'])){
-        if($nombre == "" || $empresa == "" || $responsable == "" || $fecha == ""){
+    if(isset($_GET['nombre']) || isset($_GET['empresa']) || isset($_GET['responsable']) || isset($_GET['fecha'])
+    || isset($_GET['email']) || isset($_GET['ciudad']) || isset($_GET['telefono'])){
+        if($nombre == "" || $empresa == "" || $responsable == "" || $fecha == ""
+        || $email == "" || $ciudad == "" || $telefono == ""){
             $error = true;
         }else{
             $envio = true;
@@ -25,16 +30,35 @@
         function Header()
         {
             global $title;
+            global $email;
+            global $ciudad;
+            global $telefono;
+            global $nombre;
         
             $this->SetFont('Arial','B',15);
-            $w = $this->GetStringWidth($title)+6;
+            $w = $this->GetStringWidth($nombre)+6;
             $this->SetX((210-$w)/2);
-            $this->SetDrawColor(8, 253, 216);
-            $this->SetFillColor(29, 29, 29);
-            $this->SetTextColor(8, 253, 216);
+            $this->SetTextColor(68,114,196);
             $this->SetLineWidth(1);
-            $this->Cell($w,9,$title,1,1,'C',true);
-            $this->Ln(10);
+            $this->Cell($w,9,$nombre,0,1,'C',false);
+            $this->SetDrawColor(68,114,196);
+            $this->Line(198,20,12,20);
+            $this->Ln(5);
+
+            $this->SetFont('Arial','U',12);
+            $w_e = $this->GetStringWidth($email)+6;
+            $this->SetX((210-$w_e)/2);
+            $this->Cell($w_e,6,$email,0,1,'C',false);
+
+            $this->SetFont('Arial','',12);
+            $w_c = $this->GetStringWidth($ciudad)+6;
+            $w_t = $this->GetStringWidth($telefono)+6;
+            $w_s = $this->GetStringWidth('-')+6;
+            $this->SetX((210-$w_c-$w_t-$w_s)/2);
+            $this->Cell($w_c,6,$ciudad,0,0,'R',false);
+            $this->Cell($w_s,6,'-',0,0,'C',false);
+            $this->Cell($w_t,6,$telefono,0,0,'L',false);
+            $this->Ln(15);
         }
         
         function Footer()
@@ -45,31 +69,33 @@
             $this->Cell(0,10,'Pagina '.$this->PageNo(),0,0,'C');
         }
         
-        function Section($campo, $var)
+        function Body($nombre, $empresa, $responsable, $fecha, $file)
         {
-            $this->SetFont('Arial','',12);
-            $this->Cell(35,6,"$campo:",0,0,'L',false);
-            $this->SetFont('Arial','B',12);
-            $this->Cell(50,6,"$var",0,0,'L',false);
-            $this->Ln();
+            $this->SetFont('Arial','',11);
+            $this->Cell(190,6,$empresa.", ".$fecha,0,1,'J',false);
+            $this->Ln(10);
+            $this->Cell(190,6,"Estimado Sr. $responsable",0,1,'J',false);
+            $this->Ln(2);
+            $txt = file_get_contents($file);
+            $this->MultiCell(0,5,$txt,0,'J');
+            $this->Ln(2);
+            $this->Cell(190,6,"$nombre.",0,1,'J',false);
+            
         }
         
-        function PrintSection($campo, $var)
+        function PrintBody($nombre, $empresa, $responsable, $fecha, $file)
         {
-            $this->Section($campo, $var);
+            $this->Body($nombre, $empresa, $responsable, $fecha, $file);
         }
     }
     
     $pdf = new PDF();
     
-    $title = 'Carta de recomendacion';
+    $title = 'Roman Kornyeyev';
     $pdf->SetTitle($title);
     $pdf->SetAuthor('Roman');
     $pdf->AddPage();
-    $pdf->PrintSection('Nombre', $nombre);
-    $pdf->PrintSection('Empresa', $empresa);
-    $pdf->PrintSection('Responsable', $responsable);
-    $pdf->PrintSection('Fecha', $fecha);
+    $pdf->PrintBody($nombre, $empresa, $responsable, $fecha, 'practica_10.txt');
 
     if($envio){
         $pdf->Output();
@@ -95,6 +121,20 @@
                         <input class="input input-text" type="text" name="nombre" id="nombre" value="<?= $nombre ?>" placeholder="Introduce tu NOMBRE">
                         <label class="label-lane" for="nombre"></label> 
                     </li>
+
+                    <li class="form-ul-li">
+                        <input class="input input-text" type="text" name="email" id="email" value="<?= $email ?>" placeholder="Introduce tu EMAIL">
+                        <label class="label-lane" for="email"></label> 
+                    </li>
+                    <li class="form-ul-li">
+                        <input class="input input-text" type="text" name="ciudad" id="ciudad" value="<?= $ciudad ?>" placeholder="Introduce tu CIUDAD">
+                        <label class="label-lane" for="ciudad"></label> 
+                    </li>
+                    <li class="form-ul-li">
+                        <input class="input input-text" type="text" name="telefono" id="telefono" value="<?= $telefono ?>" placeholder="Introduce la TÉLEFONO">
+                        <label class="label-lane" for="telefono"></label> 
+                    </li>
+
                     <li class="form-ul-li">
                         <input class="input input-text" type="text" name="empresa" id="empresa" value="<?= $empresa ?>" placeholder="Introduce la EMPRESA">
                         <label class="label-lane" for="empresa"></label> 
@@ -104,7 +144,7 @@
                         <label class="label-lane" for="responsable"></label> 
                     </li>
                     <li class="form-ul-li">
-                        <input class="input input-text" type="date" name="fecha" id="fecha" value="<?= $fecha ?>" placeholder="Introduce tu FECHA">
+                        <input class="input input-text" type="date" name="fecha" id="fecha" value="<?= $fecha ?>" placeholder="Introduce la FECHA">
                         <label class="label-lane" for="fecha"></label> 
                     </li>
                 </ul>
