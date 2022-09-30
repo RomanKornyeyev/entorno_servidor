2865476932b614afcd74ca097504184dc5448bb3
Román <romankr1997@gmail.com>
2022-09-30 21:30:12 +0200

    cambios ligeros

--- 1ev/practicas_arrays/clase-1.php
+++ 1ev/practicas_arrays/clase-1.php
@@ -31,6 +31,7 @@ Con el array de personas anterior, utiliza el array_filter para sacar un listado
 -->
 <?php
 
+    //=== 1.1 ====
     $personas = [
         ["Jorge", 1],
         ["Bea", 0],
@@ -56,6 +57,26 @@ Con el array de personas anterior, utiliza el array_filter para sacar un listado
 
     $lista = array_map("saludo", $personas);
 
+
+
+    //=== 1.2 ====
+    //Utiliza la función map_reduce para calcular la cantidad de calorías de la comida anterior.
+    $comida = [
+        0 => ["Banana", 3, 56],
+        1 => ["Chuleta", 1, 256],
+        2 => ["Pan", 1, 90]
+    ];
+
+    function myfunction($v1,$v2){
+        return $v1+$v2;
+    }
+    
+    
+    print_r(array_reduce($a,"myfunction",$comida[2]));
+    
+    
+    
+
 ?>
 <!DOCTYPE html>
 <html lang="es">
@@ -64,13 +85,17 @@ Con el array de personas anterior, utiliza el array_filter para sacar un listado
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- CSS -->
-    <link rel="stylesheet" href="./css/styles-arrays.css">
+    <link rel="stylesheet" href="./css/generales.css">
     <!-- JS -->
     <script src="./js/scrollreveal-lib.js"></script>
     <script src="./js/sr-2.arrays.js" defer=""></script>
     <title>Clase 1</title>
 </head>
 <body>
-    <?= imprimirArray($lista) ?>
+    <div class="container">
+        <div class="central">
+            <?= imprimirArray($lista) ?>
+        </div>
+    </div>
 </body>
 </html>
