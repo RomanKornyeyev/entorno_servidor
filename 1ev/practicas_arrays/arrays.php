147285548a8f0518d325abd5fec1bc10266cfcea
Roman <romankr1997@gmail.com>
2022-09-30 14:07:34 +0200

    ejer compas

--- 1ev/practicas_arrays/arrays.php
+++ 1ev/practicas_arrays/arrays.php
@@ -1,68 +1,30 @@
 <?php
-
-    $dwec = "Cliente";
-    $dwes = "Servidor";
-    $emp = "Empresa";
-    $ing = "Inglés";
-    $diw = "Interfaces";
-    $daw = "Despliegue";
-    
-
-    $horario = [
-        0 => ["", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes"], 
-        1 => ["16:00", $dwec, $ing, $diw, $emp, $dwes], 
-        2 => ["16:55", $dwec, $daw, $diw, $daw, $dwes], 
-        3 => ["17:50", $dwec, $daw, $diw, $daw, $dwes],
-        4 => ["18:45", "P", "A", "T", "I", "O"],
-        5 => ["19:10", $emp, $diw, $dwes, $dwes, $dwec],
-        6 => ["20:05", $emp, $diw, $dwes, $dwes, $dwec],
-        7 => ["21:50", $ing, $diw, $dwes, $dwes, $dwec],
-    ];
-
-    $estilo = [
-        $dwes => "color1",
-        $dwec => "color2",
-        $emp => "color3",
-        $ing => "color4",
-        $diw => "color5",
-        $daw => "color6",
-    ];
-
-    function generarHorario(){
-
-        global $dwec, $dwes, $emp, $ing, $diw, $daw;
-        global $estilo;
-        global $horario;
-        $cont = 1;
-        $i_aux = 0;
-
-        for ($i = 0; $i < count($horario); $i++) {
-            echo "<tr class='tr'>";
-            for($j = 0; $j < count($horario[$i]); $j++){
-                $i_aux = $i;
-                while(($i_aux+1) < count($horario) && $horario[$i_aux][$j] == $horario[$i_aux+1][$j]){ //si el td de abajo es igual, rowspan++ (cont++)
-                    $cont++;
-                    $i_aux++;
-                }
-                if($i > 0){
-                    if($horario[$i-1][$j] != $horario[$i][$j]){ //si el td de arriba es igual, no pintes nada
-                        if(in_array($horario[$i][$j], array_keys($estilo))){ //pintame el estilo extra SOLO SI COINCIDE CON UNA ASIGNATURA
-                            echo "<td class='td ".$estilo[$horario[$i][$j]]."' rowspan='$cont'>".$horario[$i][$j]."</td>";
-                        }else{ //si no coincide, pintame color-default
-                            echo "<td class='td color-default' rowspan='$cont'>".$horario[$i][$j]."</td>"; 
-                        }
-                    }
-                }else{
-                   echo "<td class='td color-default' rowspan='$cont'>".$horario[$i][$j]."</td>"; 
-                }
-                
-                $cont = 1;
-            }
-            echo "</tr>";
-        }
-    };
-
+ 
+$arrayReemplazo = [1=>20, 3=>7, 4=>13];
+ 
+$arrays = [
+  $array1 = [1,2,13,4,15,6,7,18,9],
+  $array2 = [1,12,13,14,5,6,7,18,19],
+  $arrayFusionada = array_replace(array_intersect($array1,$array2), $arrayReemplazo)
+];
+ 
+$busqueda = array_search(13, $arrayFusionada); //devolvería el 4 (el índice)
+ 
+//Función para imprimir arrays
+function imprimirArray($arr){
+ for ($i=0;$i<count($arr);$i++) {
+    for ($j=0;$j<count($arr[$i]);$j++){
+       echo "  " . $arr[$i][$j] ;
+    }
+    echo "<br>";
+ }
+}
 ?>
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -74,17 +36,19 @@
     <!-- JS -->
     <script src="./js/scrollreveal-lib.js"></script>
     <script src="./js/scrollreveal.js" defer=""></script>
-    <title>HORARIO</title>
+    <title>CRUM</title>
 </head>
 <body>
     <main class="container">
-        <h1 class="title">HORARIO</h1>
+        <h1 class="title">CRUM</h1>
         <div class="central">
-            <table class="table">
-                <tbody>
-                    <?php generarHorario() ?>
-                </tbody>
-            </table>
+        <?php
+            imprimirArray($arrays);
+            echo "¿En qué índice está el 13? - En el índice " . $busqueda;
+        ?>
+
+
+
         </div>
     </main>
 </body>
