a3776dab26a6d82878f2c56ff76527ea9bdcd7c8
Roman <romankr1997@gmail.com>
2022-10-03 04:12:13 +0200

    modularizacion

---
+++ 1ev/ejercicios/10.1.clase.php
@@ -0,0 +1,89 @@
+<!-- 
+1 Jorge (Profe)
+Funciones: array_walk, array_map, array_filter, array_reduce
+
+$personas = [
+    [Jorge, 1],
+    [Bea, 0],
+    [Paco, 1],
+    [Amparo, 0],
+];
+
+Utiliza alguna de las funciones para generar un array de cadenas indicando el nombre de la persona y cómo tratarle con formalidad. Si el valor entero detrás del nombre es un 1 Hay que poner "Señor <nombre>", si es 0 hay que poner "Señora <nombre>"
+
+$resultado = ["Señor Jorge", "Señora Bea", "Señor Paco", "Señora Amparo"];
+
+--
+
+$comida = [
+    0 => ["Banana", 3, 56],
+    1 => ["Chuleta", 1, 256],
+    2 => ["Pan", 1, 90]
+]
+    
+Utiliza la función map_reduce para calcular la cantidad de calorías de la comida anterior.
+
+--
+
+Con el array de personas anterior, utiliza el array_filter para sacar un listado de Hombre y otro listado de mujeres.
+-->
+<?php
+
+    //=== 1.1 ====
+    $personas = [
+        ["Jorge", 1],
+        ["Bea", 0],
+        ["Paco", 1],
+        ["Amparo", 0],
+    ];
+
+    function saludo($var){
+        $resultado = 0;
+        if($var[1] == 1){
+            $resultado = "Señor";
+        }else{
+            $resultado = "Señora";
+        }
+
+        return $resultado." ".$var[0];
+    }
+
+    $lista1 = array_map("saludo", $personas);
+
+
+    //=== 1.2 ====
+    //Utiliza la función array_reduce para calcular la cantidad de calorías de la comida anterior.
+    $comida = [
+        0 => ["Banana", 3, 56],
+        1 => ["Chuleta", 1, 256],
+        2 => ["Pan", 1, 90]
+    ];
+
+    function calcularCalorias($carry, $item){
+        return $carry += ($item[1] * $item[2]);
+    }
+
+    $lista2 = array_reduce($comida,"calcularCalorias");
+
+
+    //=== 1.3 ===
+    //Con el array de personas anterior, utiliza el array_filter para sacar un listado de Hombre y otro listado de mujeres.
+    function hombres($var){ //filtramos hombres
+        return ($var[1] == 1);
+    };
+    function mujeres($var){ //filtramos mujeres
+        return !($var[1] == 1);
+    };
+    //volcamos los resultados en 2 arrays
+    $listaHombres = array_filter($personas, "hombres");
+    $listaMujeres = array_filter($personas, "mujeres");
+
+    //recorremos el array e imprimimos los valores
+    function recorrerArray($valor, $llave){
+        echo "<div class='lane up-1250'>".$valor[0]."</div>";
+    }
+    //hacemos una función para llamarla en HTML
+    function walkearArray($array){
+        array_walk($array, "recorrerArray");
+    }
+?>
--- 1ev/ejercicios/10.clase.php
+++ 1ev/ejercicios/10.clase.php
@@ -1,43 +1,11 @@
-<!-- 
-1 Jorge (Profe)
-Funciones: array_walk, array_map, array_filter, array_reduce
-
-$personas = [
-    [Jorge, 1],
-    [Bea, 0],
-    [Paco, 1],
-    [Amparo, 0],
-];
-
-Utiliza alguna de las funciones para generar un array de cadenas indicando el nombre de la persona y cómo tratarle con formalidad. Si el valor entero detrás del nombre es un 1 Hay que poner "Señor <nombre>", si es 0 hay que poner "Señora <nombre>"
-
-$resultado = ["Señor Jorge", "Señora Bea", "Señor Paco", "Señora Amparo"];
-
---
-
-$comida = [
-    0 => ["Banana", 3, 56],
-    1 => ["Chuleta", 1, 256],
-    2 => ["Pan", 1, 90]
-]
-    
-Utiliza la función map_reduce para calcular la cantidad de calorías de la comida anterior.
-
---
-
-Con el array de personas anterior, utiliza el array_filter para sacar un listado de Hombre y otro listado de mujeres.
-
-
--->
 <?php
 
     $nEjercicio = 0;
 
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        if (isset($_POST['e0'])) $nEjercicio = 0; 
-        elseif (isset($_POST['e1'])) $nEjercicio = 1;
-        else $nEjercicio = 0;
-
+        if (isset($_POST['e0'])) $nEjercicio = 0; //INICIO
+        elseif (isset($_POST['e1'])) $nEjercicio = 1; //EJER 1
+        else $nEjercicio = 0; //INICIO
     }
 
     //=== FUNCIONES GENERALES ===
@@ -50,67 +18,8 @@ Con el array de personas anterior, utiliza el array_filter para sacar un listado
         }
     }
 
-    //=== 1.1 ====
-    $personas = [
-        ["Jorge", 1],
-        ["Bea", 0],
-        ["Paco", 1],
-        ["Amparo", 0],
-    ];
+    require('./10.1.clase.php');
 
-    function saludo($var){
-        $resultado = 0;
-        if($var[1] == 1){
-            $resultado = "Señor";
-        }else{
-            $resultado = "Señora";
-        }
-
-        return $resultado." ".$var[0];
-    }
-
-    $lista1 = array_map("saludo", $personas);
-    
-
-
-    //=== 1.2 ====
-    //Utiliza la función map_reduce para calcular la cantidad de calorías de la comida anterior.
-    $comida = [
-        0 => ["Banana", 3, 56],
-        1 => ["Chuleta", 1, 256],
-        2 => ["Pan", 1, 90]
-    ];
-
-    function calcularCalorias($carry, $item){
-        return $carry += ($item[1] * $item[2]);
-    }
-    
-    $lista2 = array_reduce($comida,"calcularCalorias");
-    
-
-    //=== 1.3 ===
-    //Con el array de personas anterior, utiliza el array_filter para sacar un listado de Hombre y otro listado de mujeres.
-    function hombres($var){ //filtramos hombres
-        return ($var[1] == 1);
-    };
-    function mujeres($var){ //filtramos mujeres
-        return !($var[1] == 1);
-    };
-    //volcamos los resultados en 2 arrays
-    $listaHombres = array_filter($personas, "hombres");
-    $listaMujeres = array_filter($personas, "mujeres");
-
-    //recorremos el array e imprimimos los valores
-    function recorrerArray($valor, $llave){
-        echo "<div class='lane up-1250'>".$valor[0]."</div>";
-    }
-    //hacemos una función para llamarla en HTML
-    function walkearArray($array){
-        array_walk($array, "recorrerArray");
-    }
-    
-    
-   
     
 
 ?>
@@ -125,7 +34,7 @@ Con el array de personas anterior, utiliza el array_filter para sacar un listado
     <link rel="stylesheet" href="./css/10.clase.css">
     <!-- JS -->
     <script src="./js/scrollreveal-lib.js"></script>
-    <script src="./js/sr-10.1.clase.js" defer=""></script>
+    <script src="./js/sr-10.1.clase.js" defer></script>
     <title>Clase</title>
 </head>
 <body>
@@ -133,6 +42,7 @@ Con el array de personas anterior, utiliza el array_filter para sacar un listado
     <!-- INICIO -->
     <?php if($nEjercicio == 0) { ?>
     <div class="container">
+        <h2 class="title">Ejercicios de compañeros</h2>
         <form class="formulario" action="" method="post">
             <button class="button to-left-750" type="submit" name="e1">Ejercicio 10.1</button>
             <button class="button to-left-750" type="submit" name="e2">Ejercicio 10.2</button>
