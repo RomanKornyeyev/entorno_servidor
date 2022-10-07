c4e018eedda5d882c3c10b5fc5587248cc758cee
Román <romankr1997@gmail.com>
2022-10-07 21:40:54 +0200

    algunos cambios

---
+++ 1ev/ejercicios/funciones_php/10.7.clase.php
@@ -0,0 +1,67 @@
+<!-- 
+    Función: array_rand
+
+    array_rand(array $array, int $num = 1): mixed
+    array: El array de entrada.
+    num: Especifica cuántas entradas deberían seleccionarse.
+
+    Dados dos arrays unidimensionales, uno de tareas[] y otro de personas[], asigna de manera aleatoria una tarea a cada persona.
+
+    ¿Qué ocurre si hay más tareas que minions? (un minio aleatorio deberé tener otra más)
+
+
+    === Planificación semanal ===
+
+    Si ya le echas valor, crea un arraybidimensional de tareas_diarias[dia][tarea] y haz un organigrama donde asignes tareas a cada persona durante la semana
+
+    Cada día de las semana (de lunes a viernes) se tienen que hacer todas las tareas cada día y se tienen que distribuir entre los minions.
+
+    Haz la distribución y pinta una tabla html.
+-->
+<?php
+    $tareas = [
+        'Pelar mandarinas',
+        'Comer comida',
+        'Beber bebida',
+        'Recoger título',
+        'Cobrar salario',
+        'Barrer casa',
+        'Fregar casa',
+        // Añade más
+    ];
+    
+    $minions = [
+        'Oto',
+        'Gah',
+        'Bru',
+        // Opcional
+    ];
+
+    function imprimirHorario7($tareas, $minions){
+        $random_keys = array_rand($tareas, count($tareas));
+        for ($i=0; $i < 7 ; $i++) { 
+            echo "<div> DÍA ".($i+1);
+            for ($j=0; $j < count($tareas); $j++) {
+                echo $tareas[$random_keys[$j]]." le corresponde a ".$minions[array_rand($minions, 1)];
+            }
+            echo "</div>";
+        }
+    }
+    
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/generales.css">
+    <link rel="stylesheet" href="../css/10.clase.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container grid-7 limit-width-120">
+        <?=imprimirHorario7($tareas, $minions);?>
+    </div>
+</body>
+</html>
