344ba7224d7825cf52bf8576190ca4aa5d4bd142
Román <romankr1997@gmail.com>
2022-10-24 17:51:07 +0200

    cambios ligeros

--- 1ev/ejercicios/3_funciones/js/sr-index.js
+++ 1ev/ejercicios/3_funciones/js/sr-index.js
@@ -1,16 +1,18 @@
 window.sr = ScrollReveal();
 
-sr.reveal('.elemento',{
-    duration: 1500,
-    origin: 'bottom',
-    distance: '100px',
-    easing: 'cubic-bezier(0,.42,.25,1.07)',
-    interval: 75,
-    delay: 600
-});
-
-sr.reveal('.title',{
-    duration: 1000,
-    origin: 'top',
-    distance: '100px'
+window.addEventListener('load', function(){
+    sr.reveal('.elemento',{
+        duration: 1500,
+        origin: 'bottom',
+        distance: '100px',
+        easing: 'cubic-bezier(0,.42,.25,1.07)',
+        interval: 75,
+        delay: 600
+    });
+    
+    sr.reveal('.title',{
+        duration: 1000,
+        origin: 'top',
+        distance: '100px'
+    });
 });
