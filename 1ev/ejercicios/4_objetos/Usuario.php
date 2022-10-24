461d47e140fdbed1b9b2d9f59ce54bf78bd078a4
Román <romankr1997@gmail.com>
2022-10-24 20:51:42 +0200

    ejer usuairo a medias

--- 1ev/ejercicios/4_objetos/Usuario.php
+++ 1ev/ejercicios/4_objetos/Usuario.php
@@ -5,17 +5,20 @@
         private $apellidos;
         private $deporte;
         private $nivel;
-        private $contador; 
+        private $contador;
+        private $historial;
 
         private const subirNivel = 6;
 
         //constructors
-        public function __construct($nombre = "anónimo", $apellidos = "", $deporte = null, $nivel = 0)
+        public function __construct($nombre = "anónimo", $apellidos = "", $deporte = null)
         {
             $this->nombre = $nombre;
             $this->apellidos = $apellidos;
             $this->deporte = $deporte;
-            $this->nivel = $nivel;
+            $this->nivel = 1;
+            $this->contador = 0;
+            $this->historial = [];
         }
 
         //getters and setters
@@ -28,36 +31,60 @@
         public function setNivel($nivel){$this->nivel = $nivel;}
         public function getNivel(){return $this->nivel;}
         public function setContador($contador){$this->contador = $contador;}
-        public function getContador(){return $this->contador;} 
+        public function getContador(){return $this->contador;}
+        public function setHistorial($historial){$this->historial = $historial;}
+        public function getHistorial(){return $this->historial;} 
 
         //toString
-        public function mostrar() {return "<b>Usuario:</b> ".$this->nombre." ".$this->apellidos.", deporte: ".$this->deporte.", nivel: ".$this->nivel;}
+        public function mostrar() {
+            print "<p class='azulito'>
+            <b>Usuario:</b> ".$this->nombre." ".$this->apellidos."<br>".
+            "<b>Deporte:</b> ".$this->deporte."<br>".
+            "<b>Nivel:</b> ".$this->nivel."</p>";
+            $this->mostrarHistorial();
+        }
+        public function mostrarHistorial(){
+            print "Historial:<br><ul>";
+            array_walk($this->historial, function($value, $key){
+                print "<li>".$value."</li>";
+            });
+            print "</ul>";
+        }
 
         //métodos
         public function introducirResultado(String $resultado){
             if(strcasecmp(strtolower($resultado), "victoria") == 0){
-                $contador++;
-                if($contador == self::$subirNivel){
-                    if($nivel < 6){
-                        $nivel++;
-                        echo "Felicidades, has subido de nivel ($nivel)<br>";
-                    }else echo "¡Has alcanzado el nivel máximo!<br>";
+                if(!empty($this->historial)){
+                    if($this->historial[count($this->historial)]){
+                        
+                    }
+                }
+                $this->contador++;
+                array_push($this->historial, "victoria");
+                if($this->contador == self::subirNivel){
+                    $this->contador = 0; //reset
+                    if($this->nivel < 6){
+                        $this->nivel++;
+                        print "Felicidades, has subido de nivel (al $this->nivel)<br>";
+                    }else print "¡Has alcanzado el nivel máximo!<br>";
                 }
             }else if(strcasecmp(strtolower($resultado), "derrota") == 0){
-                $contador--;
-                if($contador == self::$subirNivel){
-                    if($nivel > 1){
-                        $nivel--;
-                        echo "Felicidades, has subido de nivel ($nivel)<br>";
-                    }else echo "¡Has alcanzado el nivel máximo!<br>";
+                //$this->contador = 0;
+                $this->contador++;
+                array_push($this->historial, "derrota");
+                if($this->contador == self::subirNivel){
+                    $this->contador = 0; //reset
+                    if($this->nivel > 1){
+                        $this->nivel--;
+                        print "Felicidades, has subido de nivel ($this->nivel)<br>";
+                    }else print "¡No puedes bajar de nivel, estás en el mínimo (1)!<br>";
                 }                
             }else if(strcasecmp(strtolower($resultado), "empate") == 0){
-                $contador = 0;
+                $this->contador = 0;
+                array_push($this->historial, "empate");
             }else{
-                echo "Introduce un valor válido <br>";
+                print "Introduce un valor válido <br>";
             }
-
         }
-
     }
 ?>
--- 1ev/ejercicios/4_objetos/mainUsuario.php
+++ 1ev/ejercicios/4_objetos/mainUsuario.php
@@ -2,12 +2,74 @@
 
     require('Usuario.php');
 
-    $a = new Usuario();
+    $a = new Usuario('Román', 'Kornyeyev', 'Baloncesto');
+    $b = new Usuario('Francis', 'Gianello', 'Fútbol');
+    $c = new Usuario('Anabel', 'Pedrajas', 'Balonmano');
 
     print $a->mostrar();
-    echo "<br>";
+    print $b->mostrar();
+    print $c->mostrar();
+
+    $a->introducirResultado("victoria");
+    $a->introducirResultado("victoria");
+    $a->introducirResultado("victoria");
+    $a->introducirResultado("victoria");
+    $a->introducirResultado("victoria");
+    $a->introducirResultado("victoria");
     
-    $a->introducirResultado("victorIA");
+    $a->introducirResultado("victoria");
+    $a->introducirResultado("victoria");
+    $a->introducirResultado("victoria");
+    $a->introducirResultado("victoria");
+    $a->introducirResultado("victoria");
+    $a->introducirResultado("victoria");
+
+    $a->introducirResultado("derrota");
+    $a->introducirResultado("derrota");
+    $a->introducirResultado("derrota");
+    $a->introducirResultado("derrota");
+    $a->introducirResultado("derrota");
+    $a->introducirResultado("derrota");
+
+    $a->mostrar();
+    
+    
+    
     
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        *,*::before,*::after{
+            margin:0;
+            padding:0;
+            box-sizing:border-box;
+        }
+        body{
+            padding: 25px;
+            display: flex;
+            flex-flow: column;
+            justify-content: center;
+            align-items: center;
+        }
+        .azulito{
+            width: 90%;
+            max-width: 750px;
+            padding: 25px;
+            background-color:#69b0c7;
+            border: 1px solid gray;
+            border-bottom: none;
+        }
+        
+    </style>
+</head>
+<body>
+    
+</body>
+</html>
