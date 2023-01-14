6b0be02a389dfb7d0b3a1bc66d1795b9f456e226
Roman <romankr1997@gmail.com>
2023-01-14 16:59:36 +0100

    foro sencillo en proceso

--- 2ev/ejercicios/8.4_foro_sencillo/index.php
+++ 2ev/ejercicios/8.4_foro_sencillo/index.php
@@ -1,10 +1,18 @@
+<?php 
+
+    require('./db.php');
+
+    $resultado = $mbd->prepare("SELECT * FROM TEMAS");
+    $resultado->execute();
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ForoNoches - Inicio</title>
+    <title>ForoSencillo - Inicio</title>
 </head>
 <body>
     
