8df9e2b504d4e57ad4b6c63ff9306f06485a7cfe
Román <romankr1997@gmail.com>
2023-02-01 21:45:42 +0100

    clase miercoles 1-feb-23

--- 2ev/python/comienzosDjango/cosa/esports/urls.py
+++ 2ev/python/comienzosDjango/cosa/esports/urls.py
@@ -5,6 +5,9 @@ from . import views #del modulo esports, importame views
 
 #def
 urlpatterns = [
-    path('', views.index, name='esport_raiz')
+    path('', views.index, name='esport_raiz'),
+    path('<str:nombre>/', views.detalle_equipo, name='esport_detalle_equipo'),
+    path('juego/<str:nombre>/', views.detalle_juego, name='esport_detalle_juego'),
+    path('genero/<str:nombre>/', views.detalle_genero, name='esport_detalle_genero')
 ]
 
--- 2ev/python/comienzosDjango/cosa/esports/views.py
+++ 2ev/python/comienzosDjango/cosa/esports/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Equipo, Juego, Genero
 
 # Create your views here.
 def index(request):
     return HttpResponse('Hola Mundo')
+
+def detalle_equipo(request, nombre):
+    e = Equipo.objects.get(nombre=nombre)
+    juegos = ""
+    for j in e.juego_set.all():
+        juegos += j.nombre+"<br>"
+    
+    return HttpResponse(f"El nombre del equipo es {e.nombre}: {e.descripcion} <br> {juegos}")
+
+def detalle_juego(request, nombre):
+    return HttpResponse(f"El nombre del juego es {nombre}")
+
+def detalle_genero(request, nombre):
+    return HttpResponse(f"El nombre del genero es {nombre}")
